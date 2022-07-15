f99771a6ade7837fa7b0e872659754cb2197cd62
rajasekhara <rajasekhara@toucan-l003>
2022-07-15 17:28:26 +0530

    comit

--- Java/src/assignment3/ReverseWordAndFirstLetterCapital.java
+++ Java/src/assignment3/ReverseWordAndFirstLetterCapital.java
@@ -9,22 +9,36 @@ public class ReverseWordAndFirstLetterCapital
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the name:");
 		String name=sc.nextLine();
+		String word ;
+		String rword=" ";
+		String cname=" ";
 		String []Name =name.split(" ");
 		for(int i=0; i<Name.length; i++)
 		{
-			String word = Name[i];
-			String rword=" ";
+		    word = Name[i];
+			rword=" ";
 			for(int j=0; j<word.length(); j++)
 			{
 				char ch=word.charAt(j);
 				rword = ch+rword;
-				/*if(j==word.length())
-				{
-					ch=(word.charAt(j)).toUpperCase();
-					ch1=ch.toUppercase();
-				}*/
 			}
 			System.out.print(rword);
 		}
+		System.out.println();
+		for(int k=0; k<rword.length(); k++)
+		{
+			if(k%2==0)
+			{
+				cname=rword.substring(k+1,k+2).toUpperCase();
+		
+			}
+			else
+			{
+				cname=rword.substring(k+1,k+2).toLowerCase();
+			}
+			System.out.print(cname);
+			
+		}
+		System.out.print(cname);
 	}
 }
